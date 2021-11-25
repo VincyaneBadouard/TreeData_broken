b50a14fb69f98943c7935f7e1707973c435be1af
Camille Piponiot <camille.piponiot@gmail.com>
2021-11-25 17:00:20 +0100

    change to shiny dashboard for better UX

--- R/RunApp.R
+++ R/RunApp.R
@@ -3,7 +3,7 @@ RunApp <- function() {
   # appDir <- system.file("app", package = "TreeData")
   appDir <- "inst/app"
   if (appDir == "") {
-    stop("Could not find myapp. Try re-installing `mypackage`.", call. = FALSE)
+    stop("Could not find the Shiny app. Try re-installing the `TreeData` package.", call. = FALSE)
   }
 
   shiny::runApp(appDir, display.mode = "normal")
--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,50 +1,100 @@
-
 library(shiny)
 library(bslib)
 library(DT)
+library(shinydashboard)
+library(shinyjs)
 
-ui <- fluidPage(
+# header with title
+header <- dashboardHeader(title = "Data harmonisation")
 
-  # esthetique
-  theme = bs_theme(bootswatch = "superhero"),
-  titlePanel("Data harmonisation"),
-
-  tabsetPanel(
-
-    tabPanel("Upload",
-             sidebarLayout(
-
-               sidebarPanel(
-                 # load button for main data file (csv format)
-                 fileInput(inputId = "file1", "Choose CSV File", accept = ".csv"),
-
-                 # does the dataframe have a header?
-                 checkboxInput("header", "Header", TRUE),
-
-                 # choose separator
-                 selectInput(inputId = "cbSeparator",
-                             label = "Separator",
-                             choices = c("auto", ",", "\t",  "|", ";", ":"), # pb with tab
-                             selected = "auto"
-                 )
-               ),
-
-               mainPanel(
-                 DTOutput(outputId = "tabData"))
-             )
-    ),
-
-    tabPanel("Change format"),
-
-    tabPanel("Correct"),
-
-    tabPanel("Visualise"),
-
-    tabPanel("Save",
-             downloadButton(outputId = "dbFile", label = "Save file"),
-             downloadButton(outputId = "dbCode", label = "Save code")
-    )
-
+# sidebar contains menu items
+sidebar <- dashboardSidebar(
+  useShinyjs(),
+  sidebarMenu(menuItem("Short Manual", tabName = "Manual", icon = icon("book")),
+              menuItem("Upload your file", tabName = "Upload", icon = icon("upload")),
+              menuItem("Change format", tabName = "Reformat", icon = icon("arrows-alt")),
+              menuItem("Apply corrections", tabName = "Correct", icon = icon("check-circle")),
+              menuItem("Visualise results", tabName="Visualise", icon = icon("eye")),
+              menuItem("Save codes and data", tabName="Save", icon = icon("save"))
   )
-
 )
+
+body <- dashboardBody(
+  tabItems(
+
+    tabItem(tabName = "Upload",
+
+            fluidRow(
+
+              column(width = 3,
+
+                     # load button for main data file (csv format)
+                     fileInput(inputId = "file1", "Choose CSV File", accept = ".csv"),
+
+                     # does the dataframe have a header?
+                     checkboxInput("header", "Header", TRUE),
+
+                     # choose separator
+                     selectInput(inputId = "cbSeparator",
+                                 label = "Separator",
+                                 choices = c("auto", ",", "\t",  "|", ";", ":"), # pb with tab
+                                 selected = "auto"
+                     )
+              ),
+
+              column(width = 9,
+                     DTOutput(outputId = "tabData")
+              )
+
+            )
+    ),  ## end of "upload" panel
+
+    tabItem(tabName = "Save",
+            fluidRow(
+              downloadButton(outputId = "dbFile", label = "Save file"),
+              downloadButton(outputId = "dbCode", label = "Save code")
+            )
+    ) # end of "save" panel
+  )
+)
+
+
+dashboardPage(header, sidebar, body)
+
+# tabPanel("Upload",
+#          sidebarLayout(
+#
+#            sidebarPanel(
+#              # load button for main data file (csv format)
+#              fileInput(inputId = "file1", "Choose CSV File", accept = ".csv"),
+#
+#              # does the dataframe have a header?
+#              checkboxInput("header", "Header", TRUE),
+#
+#              # choose separator
+#              selectInput(inputId = "cbSeparator",
+#                          label = "Separator",
+#                          choices = c("auto", ",", "\t",  "|", ";", ":"), # pb with tab
+#                          selected = "auto"
+#              )
+#            ),
+#
+#            mainPanel(
+#              DTOutput(outputId = "tabData"))
+#          )
+# ),
+
+# tabPanel("Change format"),
+
+# tabPanel("Correct"),
+
+# tabPanel("Visualise"),
+#
+#     tabPanel("Save",
+#              downloadButton(outputId = "dbFile", label = "Save file"),
+#              downloadButton(outputId = "dbCode", label = "Save code")
+# )
+
+# )
+
+
