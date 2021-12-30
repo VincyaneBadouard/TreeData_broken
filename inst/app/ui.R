ab76aea1ba34fdc60d7155d6e1827279acc3d536
Valentine Herrmann <herrmannv@si.edu>
2021-12-30 08:52:34 -0500

    make sure packages are installed and loaded for any new user

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,8 +1,14 @@
-library(shiny)
-library(bslib)
-library(DT)
-library(shinydashboard)
-library(shinyjs)
+#list of packages required
+list.of.packages <- c("shiny","bslib","DT","shinydashboard","shinyjs")
+
+#checking missing packages from list
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+
+#install missing ones
+if(length(new.packages)) install.packages(new.packages, dependencies = TRUE)
+
+# load required packages' libraries
+lapply(as.list(list.of.packages), library, character.only = T)
 
 # header with title
 header <- dashboardHeader(title = "Data harmonisation")
