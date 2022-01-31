bb4cd5bfbdba5e718c70475eae80a3db6d7ce045
Vincyane Badouard <you@example.com>
2022-01-31 17:08:13 -0300

    Work on DetectErrors

--- data-raw/TestData.R
+++ data-raw/TestData.R
@@ -12,20 +12,36 @@ Data = StandardData
 ## data.frame to data.table
 setDT(Data) # with "set" "<-" is not necessary
 
-nError <- 2 # number of error to add for each case
+nError <- 1 # number of error to add for each case
 
 
-#### Missing tree size ####
-
-# Size = NA
-modif <- Data[, .I[sample(.N, nError)]] # .I = seq_len(nrow(Data)), .N = nrows in the group -> sample 2 rows number in Data
-Data[modif, DBH := NA]
-# Data[modif] # to check
-
-# Size = 0
+#### Tree size = 0 ####
 modif <- Data[, .I[sample(.N, nError)]] # .I = seq_len(nrow(Data)), .N = nrows in the group -> sample 2 rows number in Data
 Data[modif, DBH := 0]
 # Data[modif] # to check
+
+#### Missing coordinates ####
+modif <- Data[, .I[sample(.N, nError)]] # .I = seq_len(nrow(Data)), .N = nrows in the group -> sample 2 rows number in Data
+Data[modif, Xutm := NA]
+Data[modif, Yutm := NA]
+# Data[modif] # to check
+
+#### Other missing values ####
+Vars <- c("PlotArea", "Plot", "SubPlot", "CensusYear", "TreeFieldNum", "IdTree",
+          "DBH", "LifeStatus", "MeasCode", "Family", "Genus", "Species",
+          "VernName", "ScientificName", "CommercialSp")
+
+modifs <- c()
+
+for (v in 1:length(Vars)) {
+
+  modif <- Data[, .I[sample(.N, nError)]] # .I = seq_len(nrow(Data)), .N = nrows in the group -> sample 2 rows number in Data
+  Data[modif, Vars[v] := NA]
+
+  modifs <- c(modifs, modif)
+
+}
+# Data[modifs] # to check
 
 
 #### Size with bad precision (pas .0 ou .5) ####
@@ -49,26 +65,16 @@ Previous_census[IdTree == 101410] # dead
 Last_census[IdTree == 101410] # alive
 
 
-#### Missing life status ####
-
-modif <- Data[, .I[sample(.N, nError)]] # .I = seq_len(nrow(Data)), .N = nrows in the group -> sample 2 rows number in Data
-Data[modif, LifeStatus := NA]
-Data[modif] # to check
-
-
 #### Duplicated TreeFieldNum in plot-subplot association ####
 
 modif <- Data[, .I[sample(.N, 1)]] # 1 row to change
-duplicatedFieldNum <- Data[!(row.names(Data)) %in% modif & # rows != modif
+duplicatedFieldNum <- Data[!(row.names(Data)) %in% modif & # all rows != modif
                              Plot == Data[modif, Plot] & # same plot as modif
                              SubPlot == Data[modif, SubPlot], # same subplot as modif
                            sample(TreeFieldNum,1)] # 1 TreeFieldNum to duplicate
 
 Data[modif, TreeFieldNum := duplicatedFieldNum] # on the row to modif, we duplicate the TreeFieldNum
 # Data[TreeFieldNum == duplicatedFieldNum] # to check
-
-
-#### Unseen tree but alive tree after ####
 
 #### Duplicated IdTree in a census ####
 
@@ -80,7 +86,7 @@ Data[IdTree %in% idModif, IdTree := duplicatedID] # we duplicate the IdTree on t
 
 Data[CensusYear == 2020 & IdTree == duplicatedID] # to check
 
-
+#### Unseen tree but alive tree after ####
 
 #### Abnomal growth ####
 #### Abnomal recruit ####
