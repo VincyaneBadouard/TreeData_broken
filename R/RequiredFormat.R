760a2959ca1fcf6ad714afc7a679d5783a7dd866
ValentineHerr <herrmannv@si.edu>
2022-02-10 11:00:01 -0500

    make sure x is found (probably need to update this to a more elegant way...)

--- R/RequiredFormat.R
+++ R/RequiredFormat.R
@@ -63,7 +63,12 @@ RequiredFormat <- function(
   # Load interactive items to see what we are missing ####
 
   if(!ThisIsShinyApp) {
-  x <- read.csv("inst/app/data/interactive_items.csv")
+  x <- try(expr = read.csv(system.file("/app/data/", "interactive_items.csv", package = "TreeData", mustWork = TRUE)), silent = T)
+
+  if (class(x) %in% "try-error"){
+     warning("TreeData package not loaded. Assuming you are in the root of the package instead.")
+    x <- read.csv("inst/app/data/interactive_items.csv")
+  }
   x <- x[x$Activate,]
   }
 
