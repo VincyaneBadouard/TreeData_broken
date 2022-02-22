7c7538c4aca2cfe4ec2b5450cf8f785c71e48c2b
ValentineHerr <herrmannv@si.edu>
2022-02-22 09:53:45 -0500

    update app

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -5,10 +5,10 @@ list.of.packages <- c("shiny","bslib","DT","shinydashboard","shinyjs", "shinyWid
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 
 #install missing ones
-if(length(new.packages)) install.packages(new.packages, dependencies = TRUE)
+# if(length(new.packages)) install.packages(new.packages, dependencies = TRUE)
 
 # load required packages' libraries
-lapply(as.list(list.of.packages), library, character.only = T)
+lapply(as.list(list.of.packages), require, character.only = T)
 
 
 
