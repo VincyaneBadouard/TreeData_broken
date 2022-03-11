b3992a77a4bc5cded4e2c0e1eba09299121a7aac
ValentineHerr <herrmannv@si.edu>
2022-03-11 10:48:25 -0500

    finalize app to maybe get it tested?
    @gabrielareto I am going to try to publish this version of the app and see if someone wants to give some feedback and maybe create a first profile. (Note that I have been having issues with publishing so it may take a bit of time to figure it out)

--- inst/app/server.R
+++ inst/app/server.R
@@ -1,24 +1,15 @@
 
-# Fichier pour gérer les interactions de l'application Shiny
-
 # source script to get VegX_tree
-# source("data/My_VegX.R")
 VegXtree <- readRDS("data/VegXtree.rds")
 VegXtree$Do(function(x) x$inputId <-gsub("/", "_", x$pathString))
 
-# tree <- ToListExplicit(VegXtree)$children
 tree <- cbind(ToDataFrameTypeCol(VegXtree), ToDataFrameTable(VegXtree, "name", "inputId", "annotation"))
 
 tree$subtext <- paste("<strong>", tree$name, "</strong>", stringr::str_replace_all( stringr::str_wrap(tree$annotation, width = 50), "\\n", "<br>"))
-# tree <- tree[tree$name != "NULL",] # remove "NULL names
 tree <- split(tree, factor(tree$level_2, levels = unique(tree$level_2)))
 
 choices <- sapply(tree, function(x) setNames(as.list(gsub("VegX_", "", x$inputId)), x$name))
 subtext <- unlist(sapply(tree, "[[", "subtext"), recursive = T)
-
-# subtext <- unlist(sapply(tree, "[[", "annotation"))
-# subtext <- stringr::str_wrap(subtext, width = 75)
-# subtext <- paste("<strong>", unlist(choices), "</strong>", stringr::str_replace_all(subtext, "\\n", "<br>"))
 
 # level3 <-  sapply(tree, "[[", "level_3")
 # choices <- mapply(FUN = function(x, y) split(x, factor(y, levels = unique(y))), x = choices, y = level3)
@@ -37,7 +28,7 @@ server <- function(input, output, session) {
 
                      column(width = 3,
                             # load button for main data file (csv format)
-                            box(title = paste("Upload your table", i),
+                            box(title = paste("Table", i),
                                 width = NULL,
                                 fileInput(inputId = paste0("file", i), "Choose CSV File", accept = ".csv"),
                                 # does the dataframe have a header?
@@ -49,9 +40,18 @@ server <- function(input, output, session) {
                                             selected = "auto"
                                 ),
                                 textInput(inputId = paste0("TableName", i),
-                                          label = "Give an explicit UNIQUE name to this table. No space, no special character, no accent.",
+                                          label = "Give an explicit UNIQUE and SHORT name to this table. No space, no special character, no accent.",
                                           value = paste0("Table", i)
-                                )
+                                ),
+                                # textInput(inputId = paste0("TableDescription", i),
+                                #           label = "Give an explicit description of your table.",
+                                #           value = paste0("My Table", i)
+                                # ),
+                                tags$textarea(id=paste0("TableDescription", i),
+                                              label = "Give an explicit description of your table...",
+                                              rows=5,
+                                              cols = 6,
+                                              placeholder = paste0("My Table", i))
                             )
                      )
 
@@ -114,35 +114,6 @@ server <- function(input, output, session) {
     NoData = "No data has been submitted yet."
   )
 
-
-
-  # observe({
-  #
-  #
-  #
-  #   output$uiheader <- renderUI({
-  #     choices <-  isolate(unlist(sapply(names(Data()), function(x) setNames(paste(x, colnames(Data()[[x]]), sep = "_"), paste(x, colnames(Data()[[x]]), sep = "_")), simplify = FALSE)))
-  #     lapply(tree, function(x) {
-  #     div(h2(x$level_2[1]),
-  #         lapply(1:nrow(x), function(i) {
-  #           pickerInput(inputId = x$inputId[i] ,
-  #                       label =  div(h4(x$name[i]), p(x$annotation[i])),
-  #                       choices = choices,
-  #                       inline = T)
-  #         })
-  #     )
-  #
-  #
-  #   })})
-  #
-  #
-  #   })
-
-
-
-
-
-
   observeEvent(input$submit, {
   output$uiheader <- renderUI({
 
@@ -160,133 +131,33 @@ server <- function(input, output, session) {
   })
 
   updateTabItems(session, "tabs", "headers")
+
+
   })
 
 
 
-
-  # observeEvent(Data(), {output$test <- renderText({unlist(sapply(names(Data()), function(x) paste(x, colnames(Data()[[x]]), sep = "_")))})})
-  # observeEvent(Data(), {output$test <-renderText({ paste(paste(names(Data())[1], colnames(Data()[[1]]), sep = "_"), collapse = "") })})
-  # observeEvent(Data(), {output$test <-renderText({ column_names() })})
-
-  # create options to choose from:
-
-  ColumnOptions <- eventReactive(Data(), { c("none", colnames(Data())) })
-
-  UnitOptions <- eventReactive(Data(),
-                               {c("mm", "cm", "dm", "m")
-                               })
-
-  AreaUnitOptions <- eventReactive(Data(),
-                                   {c("m2", "ha", "km2")
-                                   })
-
-
-  LifeStatusOptions <- eventReactive(input$LifeStatus, {
-    sort(unique(Data()[[input$LifeStatus]]))})
-
-  CommercialOptions <- eventReactive(input$CommercialSp, {
-    sort(unique(Data()[[input$CommercialSp]]))})
-
-  OtherOptions <- eventReactive(Data(), {""})
-
-  # enter column names for each element of the RequiredFormat function
-
-  observeEvent(input$file0,
-               {
-                 output$ui1 <- renderUI({
-                   lapply(1:nrow(x1), function(i) {
-
-                     eval(parse(text = paste(x1$ItemType[i], "(inputId = x1$ItemID[i], label = ifelse(x1$helpText[i] %in% '', x1$Label[i], paste0(x1$Label[i], ' (', x1$helpText[i], ')')),", x1$argument[i],"= get(x1$argValue[i])()", ifelse(x1$Options[i] != FALSE, paste0(", options = ", x1$Options[i]), ""), ifelse(x1$Multiple[i] %in% TRUE, ", multiple = TRUE)", ")"))))
-
-                   })
-
-                 })
-
-                 output$ui2 <- renderUI({
-
-                   lapply(c(1:nrow(x2)), function(i) {
-                     if(input[[x2$if_X1_is_none[i]]] %in% "none")
-                       eval(parse(text = paste(x2$ItemType[i], "(inputId = x2$ItemID[i], label = ifelse(x2$helpText[i] %in% '', x2$Label[i], paste0(x2$Label[i], ' (', x2$helpText[i], ')')),", x2$argument[i], "= get(x2$argValue[i])()", ifelse(x2$Options[i] != FALSE, paste0(", options = ", x2$Options[i]), ""), ifelse(x2$Multiple[i] %in% TRUE, ", multiple = TRUE)", ")"))))
-
-                   })
-                 })
-
-                 output$ui3 <- renderUI({
-
-                   lapply(c(1:nrow(x3)), function(i) {
-                     if(input[[x3$if_X1_is_none[i]]] %in% "none" & !input[[x3$if_X2_isnot_none[i]]] %in% "none" )
-
-                       eval(parse(text = paste(x3$ItemType[i], "(inputId = x3$ItemID[i], label = ifelse(x3$helpText[i] %in% '', x3$Label[i], paste0(x3$Label[i], ' (', x3$helpText[i], ')')),", x3$argument[i], "= get(x3$argValue[i])()", ifelse(x3$Options[i] != FALSE, paste0(", options = ", x3$Options[i]), ""), ifelse(x3$Multiple[i] %in% TRUE, ", multiple = TRUE)", ")"))))
-
-                   })
-                 })
-
-                 output$ui4 <- renderUI({
-
-                   lapply(c(1:nrow(x4)), function(i) {
-                     if(!input[[x4$if_X2_isnot_none[i]]] %in% "none" )
-                       eval(parse(text = paste(x4$ItemType[i], "(inputId = x4$ItemID[i], label = ifelse(x4$helpText[i] %in% '', x4$Label[i], paste0(x4$Label[i], ' (', x4$helpText[i], ')')),", x4$argument[i], "= get(x4$argValue[i])()", ifelse(x4$Options[i] != FALSE, paste0(", options = ", x4$Options[i]), ""), ifelse(x4$Multiple[i] %in% TRUE, ", multiple = TRUE)", ")"))))
-
-                   })
-                 })
-
-                 output$ui5 <- renderUI({
-
-                   lapply(c(1:nrow(x5)), function(i) {
-                     if(input[[x5$if_X1_is_none[i]]] %in% "none" & input[[x5$if_X2_is_none[i]]] %in% "none" )
-
-                       eval(parse(text = paste(x5$ItemType[i], "(inputId = x5$ItemID[i], label = ifelse(x5$helpText[i] %in% '', x5$Label[i], paste0(x5$Label[i], ' (', x5$helpText[i], ')')),", x5$argument[i], "= get(x5$argValue[i])()", ifelse(x5$Options[i] != FALSE, paste0(", options = ", x5$Options[i]), ""), ifelse(x5$Multiple[i] %in% TRUE, ", multiple = TRUE)", ")"))))
-
-                   })
-                 })
-
-               })
-
-
-  # format data usin the input
-
-  DataFormated <- eventReactive(input$LaunchFormating | input$UpdateTable, {
-
-    tryCatch({
-      RequiredFormat(Data = Data(), isolate(reactiveValuesToList(input)), x, ThisIsShinyApp = T)
-    },
-    warning = function(warn){
-      showNotification(gsub("in RequiredFormat\\(Data = Data\\(\\), isolate\\(reactiveValuesToList\\(input\\)\\),", "", warn), type = 'warning', duration = NULL)
-    },
-    error = function(err){
-      showNotification(gsub("in RequiredFormat\\(Data = Data\\(\\), isolate\\(reactiveValuesToList\\(input\\)\\),", "", err), type = 'err', duration = NULL)
-    })
-  })
 
   # Visualize output
-  output$tabDataFormated <- renderDT({
-    # validate(
-    #   need(req(DataFormated()), "AA")
-    # )
-    DataFormated()
-  }, rownames = FALSE,
-  options = list(pageLength = 8, scrollX=TRUE))
+ observeEvent(input$update, {
 
-  # save final data table
+  X <- isolate(unlist(sapply(1:input$nTable, function(i) paste(reactiveValuesToList(input)[paste0("TableName", i)], names(Data()[[i]]), sep = "_"))))
+  value_entered <- sapply(reactiveValuesToList(input)[X], paste, collapse = ";")
+  inputValues <- data.frame(input = names(value_entered), value_entered )
+  inputValues <- inputValues[inputValues$value_entered != "",]
+  output$visualiseInput <- renderTable(inputValues)
 
-  output$dbFile <- downloadHandler(
-    filename = function() {
-      paste(gsub(".csv", "", input$file0$name), '_formated.csv', sep = '')
-    },
-    content = function(file) {
-      write.csv(DataFormated(), file, row.names = FALSE)
-    }
-  )
+  })
+
 
   # save profile Rdata file
 
   output$dbProfile <- downloadHandler(
     filename = function() {
-      paste(gsub(".csv", "", input$file0$name), '_Profile.rds', sep = '')
+      paste("My_Profile_", Sys.Date(), ".rds", sep = '')
     },
     content = function(file) {
-      inputs_to_save <- names(input)[names(input) %in% x$ItemID]
+      inputs_to_save <- names(input)#[names(input) %in% x$ItemID]
       Profile <- list()
       for(input.i in inputs_to_save){
         Profile[[input.i]] <-  input[[input.i]]
@@ -295,45 +166,5 @@ server <- function(input, output, session) {
     }
   )
 
-  # save code needed to produce the table
-
-  output$dbCode <- downloadHandler(
-    filename = function() {
-      paste(gsub(".csv", "",input$file0$name), '_Code.R', sep = '')
-    },
-    content = function(file) {
-      text_upload <- glue::glue(
-        "
-      # install TreeData package
-      githubinstall::githubinstall('VincyaneBadouard/TreeData')
-      library(TreeData)
-
-      # upload the data
-       Data <- data.table::fread('{input$file0$name}', header = {input$header}, sep = '{input$cbSeparator}')
-
-      # upload your profile (saved via shiny app)
-      Profile <- readRDS(paste0(gsub('.csv', '', '{input$file0$name}'), '_Profile.rds'))
-
-      # format your data
-      DataFormated <- ParacouSubsetFormated <- RequiredFormat( Data, input = Profile)
-      ")
-      writeLines(text_upload, file)
-    }
-  )
-
-  # Save metadata
-
-  output$dbMetadata <- downloadHandler(
-    filename = function() {
-      paste(gsub(".csv", "", input$file0$name), '_Metadata.csv', sep = '')
-    },
-    content = function(file) {
-      columns_to_save <- colnames(DataFormated())
-      Metadata <- data.frame(Field = columns_to_save,
-                             Description = x$Description[match(columns_to_save, x$ItemID)])
-
-      write.csv(Metadata, file = file, row.names = F)
-    }
-  )
 
 }
--- inst/app/ui.R
+++ inst/app/ui.R
@@ -10,42 +10,6 @@ new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"
 # load required packages' libraries
 lapply(as.list(list.of.packages), require, character.only = T)
 
-# source script to get VegX_tree
-# source("data/My_VegX.R")
-VegXtree <- readRDS("data/VegXtree.rds")
-VegXtree$Do(function(x) x$inputId <- gsub("/", "_", x$pathString))
-
-# tree <- ToListExplicit(VegXtree)
-tree <- cbind(ToDataFrameTypeCol(VegXtree), ToDataFrameTable(VegXtree, "name", "inputId", "annotation"))
-tree <- split(tree, factor(tree$level_2, levels = unique(tree$level_2)))
-
-my_pickerInput <- function(x) {
-  pickerInput(inputId = x$itemID ,
-              label =  div(h3(x$name2), p(x$annotation)),
-              choices = "")
-}
-
-my_lapply <- function(x) {
-  lapply(x, function(y) {
-    if(length(y) ==1) my_pickerInput(y) else my_dropdown(y)
-  })
-}
-
-
-my_dropdown <-     function(x) {
-  div(tags$h3(x$name), dropdown(
-    circle = F,
-    tooltip = T,
-
-    my_lapply(x[-1])
-
-  ))
-
-}
-
-
-
-
 # header with title
 header <- dashboardHeader(title = "Data harmonisation")
 
@@ -55,10 +19,10 @@ sidebar <- dashboardSidebar(
   sidebarMenu(id = "tabs",
               menuItem("Upload your file(s)", tabName = "Upload", icon = icon("upload")),
               menuItem("Identify headers", tabName = "headers", icon = icon("arrows-alt")),
-              menuItem("Apply corrections", tabName = "Correct", icon = icon("check-circle")),
-              menuItem("Visualise results", tabName="Visualise", icon = icon("eye")),
-              menuItem("Save codes and data", tabName="Save", icon = icon("save")),
-              menuItem("Help", tabName = "Manual", icon = icon("book"))
+              # menuItem("Apply corrections", tabName = "Correct", icon = icon("check-circle")),
+              menuItem("Visualise inputs", tabName="Visualise", icon = icon("eye")),
+              menuItem("Save Profile", tabName="Save", icon = icon("save"))#,
+              # menuItem("Help", tabName = "Manual", icon = icon("book"))
   )
 )
 
@@ -78,52 +42,54 @@ body <- dashboardBody(
     tabItem(tabName = "Upload",
 
             fluidRow(
+              column(width = 6,
+                     actionBttn(
+                       inputId = "inactivebutton",
+                       label = "1",
+                       style = "pill",
+                       color = "danger"),
+                     strong("How many tables do you wish to upload?"),
+                     numericInput(inputId = "nTable",
+                                  label = "",
+                                  value = 1,
+                                  min = 1,
+                                  max = NA
+                     )
+              )),
+
+            fluidRow(
+              column(width = 12,
+                     actionBttn(
+                       inputId = "inactivebutton",
+                       label = "2",
+                       style = "pill",
+                       color = "danger"),
+                     strong("Upload your tables"),
+              uiOutput("ui_uploadTables"))),
+
+
+            fluidRow(
               column(width = 3,
-              numericInput(inputId = "nTable",
-                          label = "How many tables do you wish to upload?",
-                          value = 1,
-                          min = 1,
-                          max = NA
-                          )
-              ),
-              uiOutput("ui_uploadTables"),
-              actionBttn(
-                inputId = "submit",
-                label = "submit",
-                style = "material-flat",
-                color = "success"
-              )
+                     actionBttn(
+                       inputId = "inactivebutton",
+                       label = "3",
+                       style = "pill",
+                       color = "danger"),
+                     actionBttn(
+                       inputId = "submit",
+                       label = "submit",
+                       style = "material-flat",
+                       color = "success"
+                     ))
 
             )
 
-            #   column(width = 3,
-            #
-            #          # load button for main data file (csv format)
-            #          box(title = "Upload your data",
-            #              width = NULL,
-            #              fileInput(inputId = "file0", "Choose CSV File", accept = ".csv"),
-            #              # does the dataframe have a header?
-            #              checkboxInput("header", "Header", TRUE),
-            #              # choose separator
-            #              selectInput(inputId = "cbSeparator",
-            #                          label = "Separator",
-            #                          choices = c("auto", ",", "\t",  "|", ";", ":"), # pb with tab
-            #                          selected = "auto"
-            #              )
-            #          )
-            #   ),
-            #
-            #   column(width = 9,
-            #          DTOutput(outputId = "tabData")
-            #   )
-            #
-            # )
     ),  ## end of "upload" panel
 
     tabItem(tabName = "headers",
             fluidRow(
               column(width = 12,
-                     h5("This may take a few seconds to show..."),
+                     h5("This may take a few seconds to show... BUT MAKE SURE YOU CLICKED ON 'SUBMIT' ON PREVIOUS TAB!"),
                      br(),
                      h4("In the menus below, select the option(s) that apply to each of your columns."),
                      p("Multiple choice allowed -  To clear a field, deselect all of the options."),
@@ -132,18 +98,23 @@ body <- dashboardBody(
             )
     ,
 
-    tabItem(tabName = "Correct",
-            radioButtons(inputId = "taper", label = "Apply taper corrections?", choices = list("Yes" = "Yes", "No" = "No"), selected = "No")
-            ),
+    # tabItem(tabName = "Correct",
+    #         radioButtons(inputId = "taper", label = "Apply taper corrections?", choices = list("Yes" = "Yes", "No" = "No"), selected = "No")
+    #         ),
 
     tabItem(tabName = "Visualise",
 
             fluidRow(
+                     actionBttn(
+                       inputId = "update",
+                       label = "update",
+                       style = "material-flat",
+                       color = "success"
+                     ),
+                     tableOutput("visualiseInput")
 
-              column(width = 10,
-                     DTOutput(outputId = "tabDataFormated")
-              ),
-              actionButton("UpdateTable", label = "Update table!", style = "color: #fff; background-color: #009e60; border-color: #317256;   position: fixed")
+              # uiOutput(outputId = "visualiseInput")
+              # actionButton("UpdateTable", label = "Update table!", style = "color: #fff; background-color: #009e60; border-color: #317256;   position: fixed")
             )
 
 
@@ -152,26 +123,26 @@ body <- dashboardBody(
     tabItem(tabName = "Save",
             fluidRow(
               column(width = 3,
-                     box(title = "Save file",
-                         width = NULL,
-                         status = "primary",
-                         solidHeader = TRUE,
-                         downloadButton(outputId = "dbFile", label = "Save file")),
+                     # box(title = "Save file",
+                     #     width = NULL,
+                     #     status = "primary",
+                     #     solidHeader = TRUE,
+                     #     downloadButton(outputId = "dbFile", label = "Save file")),
                      box(title = "Save profile",
                          width = NULL,
                          status = "primary",
                          solidHeader = TRUE,
-                         downloadButton(outputId = "dbProfile", label = "Save profile")),
-                     box(title = "Save code",
-                         width = NULL,
-                         status = "primary",
-                         solidHeader = TRUE,
-                         downloadButton(outputId = "dbCode", label = "Save code")),
-                     box(title = "Save metadata",
-                         width = NULL,
-                         status = "primary",
-                         solidHeader = TRUE,
-                         downloadButton(outputId = "dbMetadata", label = "Save metadata"))
+                         downloadButton(outputId = "dbProfile", label = "Save profile"))#,
+                     # box(title = "Save code",
+                     #     width = NULL,
+                     #     status = "primary",
+                     #     solidHeader = TRUE,
+                     #     downloadButton(outputId = "dbCode", label = "Save code")),
+                     # box(title = "Save metadata",
+                     #     width = NULL,
+                     #     status = "primary",
+                     #     solidHeader = TRUE,
+                     #     downloadButton(outputId = "dbMetadata", label = "Save metadata"))
               )
             )
     ) # end of "save" panel
