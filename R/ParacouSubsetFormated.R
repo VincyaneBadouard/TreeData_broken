3d8c7e0ca6faa4c79515424cd428372582e3c3c9
ValentineHerr <herrmannv@si.edu>
2022-05-11 10:12:23 -0400

    update ParacouSubsetFormated

--- R/ParacouSubsetFormated.R
+++ R/ParacouSubsetFormated.R
@@ -7,27 +7,31 @@
 #'   \item{Site}{Site level name}
 #'   \item{Plot}{Plot level name}
 #'   \item{SubPlot}{SubPlot level name}
+#'   \item{PlotArea}{Size of the plot in ha}
+#'   \item{SubPlotArea}{Size of the plot in ha}
+#'   \item{IdCensus}{Year of measurement}
+#'   \item{Year}{Year of measurement}
+#'   \item{Date, DateOriginal}{Date of measurement}
 #'   \item{TreeFieldNum}{Tree unique identifiers, matching the tag number in the field if that column was provided in the input, automatically generated if not.}
 #'   \item{IdTree}{Tree unique identifiers. This was automatically generated if not provided in the input data}
-#'   \item{Xfield}{Tree X euclidean position in plot in m}
-#'   \item{Yfield}{Tree Y euclidean position in plot in m}
-#'   \item{Xutm}{X UTM coordinates in m}
-#'   \item{Yutm}{Y UTM coordinats in m}
+#'   \item{LifeStatus, LifeStatusOriginal}{Logical: TRUE is tree was alive, FALSE if it was dead}
+#'   \item{DBH}{Diameter at breast height in cm}
+#'   \item{Circ}{circumference in cm}
+#'   \item{POM}{Height of measurement in m}
 #'   \item{Lat}{Latitude in degrees}
 #'   \item{Lon}{Longitude in degrees}
+#'   \item{Xutm}{X UTM coordinates in m}
+#'   \item{Yutm}{Y UTM coordinats in m}
+#'   \item{Xfield}{Tree X euclidean position in plot in m}
+#'   \item{Yfield}{Tree Y euclidean position in plot in m}
+#'   \item{ScientificName}{Scientific name}
+#'   \item{VernName}{Vernacular name}
 #'   \item{Family}{Family name}
 #'   \item{Genus}{Genus name}
 #'   \item{Species}{Species name}
-#'   \item{VernName}{Vernacular name}
-#'   \item{CommercialSp}{Logical: TRUE if species is considered commercial, FALSE if not}
-#'   \item{CensusYear}{Census Year}
-#'   \item{LifeStatus}{Logical: TRUE is tree was alive, FALSE if it was dead}
-#'   \item{Circ}{circumference in cm}
-#'   \item{IdStem}{Stem unique identifier}
-#'   \item{IdMeasure}{measure unique identifier if you had more than one measure per stem per year}
-#'   \item{DBH}{Diameter at breast height in cm}
-#'   \item{POM}{Height of measurement in m}
-#'   \item{ScientificName}{Scientific name}
+#'   \item{IdLevel}{Logical: TRUE if species is considered commercial, FALSE if not}
+#'   \item{Authority}{Logical: TRUE if species is considered commercial, FALSE if not}
+#'   \item{CommercialSp, CommercialSpOriginal}{Logical: TRUE if species is considered commercial, FALSE if not}
 #'   \item{TreeHeight}{Tree Height in m}
 #'   \item{LifeForm}{Life form}
 #'   ...
--- data-raw/ParacouSubsetFormated.R
+++ data-raw/ParacouSubsetFormated.R
@@ -13,7 +13,7 @@ usethis::use_R(ParacouSubsetFormated, overwrite = TRUE)
 
 ## For ParacouProfileFormated.Rmd  run next two line of code and paste in the item section of R/ParacouProfile.R
 x <- read.csv("inst/app/data/interactive_items.csv")
-
+idx = names(ParacouSubsetFormated) %in% x$ItemID
 write.csv(
-  paste0("#'   \\item{", names(ParacouSubsetFormated), "}{", x$Description[match(names(ParacouSubsetFormated), x$ItemID)], "}"), "clipboard",
+  paste0("#'   \\item{", names(ParacouSubsetFormated)[idx], ifelse(paste0(names(ParacouSubsetFormated)[idx], "Original") %in% names(ParacouSubsetFormated), paste0(", ", names(ParacouSubsetFormated)[idx], "Original"), ""), "}{", x$Description[match(names(ParacouSubsetFormated)[idx], x$ItemID)], "}"), "clipboard",
   quote = F, row.names = F)
