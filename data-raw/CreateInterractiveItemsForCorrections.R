4a7781161f3eab42decf83d5c4847a05efcb3227
ValentineHerr <herrmannv@si.edu>
2022-07-12 18:55:54 -0400

    update interactive Correction Functions

--- data-raw/CreateInterractiveItemsForCorrections.R
+++ data-raw/CreateInterractiveItemsForCorrections.R
@@ -1,4 +1,3 @@
-
 # get the Rd from the package
 db <- tools::Rd_db("TreeData")
 
@@ -19,7 +18,7 @@ Fct_args <- do.call(rbind, lapply(Fct_args, function(y) do.call(rbind, lapply(y,
 Fct_args <- Fct_args[!Fct_args$Label %in% "Data",] # remove Data
 
 # add a column to say what function it is coming from
-Fct_args$Function <- gsub("\\.Rd\\.\\d", "", rownames(Fct_args))
+Fct_args$Function <- gsub("\\.Rd\\.\\d*", "", rownames(Fct_args))
 
 # create ItemID
 Fct_args$ItemID <- paste0(Fct_args$Function, Fct_args$Label)
@@ -39,18 +38,74 @@ for(f in unique(Fct_args$Function)) {
 # ItemTypeFunction <- function(class) switch(class, "numeric" = "numericInput", "logical" = "pickerInput", "call" = "pickerInput", "text" = "textInput")
 
 df_moreInfo <-
-data.frame(class = c("numeric", "logical", "call","text"),
-           ItemType = c("numericInput", "pickerInput", "pickerInput", "textInput"),
-           Multiple = c(F, F, T, F),
-           Options = c(FALSE, "list(`live-search` = TRUE)",  "list( `actions-box` = TRUE)", FALSE),
-           argument = c("value", "choices", "choices", "value"),
-           argument2 = c(FALSE, "selected", "selected", FALSE),
-           argValue = c("OtherOptions", "LogicalOptions", "FormatedColumnOptions", "OtherOptions"))
+data.frame(class = c("numeric numeric", "integer integer", "logical logical", "character call","character character", "NULL NULL", "numeric call", "function call"),
+           ItemType = c("numericInput", "numericInput","pickerInput", "pickerInput", "textInput", "TBD", "numericInput", "pickerInput"),
+           Multiple = c(F, F, F, T, F, "TBD", F, F),
+           Options = c(F, F, "list(`live-search` = TRUE)",  "list( `actions-box` = TRUE)", F, "TBD", F, "list( `actions-box` = TRUE)"),
+           argument = c("value", "value", "choices", "choices", "value", "TBD", "value", "choices"),
+           argument2 = c(F, F, "selected", "selected", F, "TBD", F, "selected"),
+           argValue = c("OtherOptions", "OtherOptions", "LogicalOptions", "TBD", "OtherOptions", "TBD", "OtherOptions", "Function")) # TBD because some need to be column names, others need to be list of options given by default.
 
 
+Fct_args <- cbind(Fct_args, df_moreInfo[match(sapply(Fct_args$Default, function(x) paste(class(eval(x)), class(x))), df_moreInfo$class), -1], ReactiveArgValue
+= T) # by default is a reactive in the app, but when list of option defined in the function, it will be changed to FALSE
+
+## get the list of columns
+x <- read.csv("inst/app/data/interactive_items.csv")
 
 
-Fct_args <- cbind(Fct_args, df_moreInfo[match(sapply(Fct_args$Default,class), df_moreInfo$class), -1])
+for(i in which(Fct_args$ItemType %in% "TBD")) {
+
+
+  if(grepl("ScientificName", Fct_args$helpText[i])) {
+    Fct_args$ItemType[i] <- "pickerInput"
+    Fct_args$Multiple[i] <- T
+    Fct_args$Options[i] <- "list( `actions-box` = TRUE)"
+    Fct_args$argument[i] <- "choices"
+    Fct_args$argument2[i] <- "selected"
+    Fct_args$argValue[i] <- "FormatedScientificNameOptions"
+    Fct_args$ReactiveArgValue[i] <- TRUE
+
+  }
+
+}
+
+for(i in which(Fct_args$argValue %in% "TBD")) {
+
+
+  if(all(eval( Fct_args$Default[[i]]) %in% x$ItemID[x$Activate == T])) {
+    Fct_args$argValue[i] <- "FormatedColumnOptions"
+    Fct_args$ReactiveArgValue[i] <- TRUE
+  }
+
+  if(!all(eval( Fct_args$Default[[i]]) %in% x$ItemID[x$Activate == T])){
+    Fct_args$argValue[i] <- deparse(Fct_args$Default[[i]])
+    Fct_args$ReactiveArgValue[i] <- FALSE
+    Fct_args$Default[i] <- paste0('c("', eval(Fct_args$Default[[i]])[1], '")')
+    }
+
+}
+
+for(i in which(Fct_args$argValue %in% "TBD")) {
+
+
+  if(all(eval( Fct_args$Default[[i]]) %in% x$ItemID[x$Activate == T]))  Fct_args$argValue[i] <-"FormatedColumnOptions"
+
+  if(!all(eval( Fct_args$Default[[i]]) %in% x$ItemID[x$Activate == T])) Fct_args$argValue[i] <- deparse(Fct_args$Default[[i]])
+
+
+ Fct_args$ReactiveArgValue[i] <- ifelse(!all(eval( Fct_args$Default[[i]]) %in% x$ItemID[x$Activate == T]), FALSE, TRUE)
+
+ if(! Fct_args$ReactiveArgValue[i])  Fct_args$Default[i] <- paste0('c("', eval(Fct_args$Default[[i]])[1], '")')
+
+}
+
+for(i in which(Fct_args$argValue %in% "Function")) {
+  Fct_args$argValue[i] <- paste0('list("Default Function" = "', paste(deparse(Fct_args$Default[[i]]), collapse = ""), '")')
+  Fct_args$Default[i] <- paste0('list("Default Function" = "', paste(deparse(Fct_args$Default[[i]]), collapse = ""), '")')
+
+  Fct_args$ReactiveArgValue[i] <- FALSE
+}
 
 
 # consider default as character now
